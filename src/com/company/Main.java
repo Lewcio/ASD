88e816ef9da05fa1a3e63001c61aa56593465c34
Michał Lewandowski <lewy@Michas-MacBook-Pro.local>
2018-01-16 19:39:26 +0100

    selection sort

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
     public static int binarySearch(int tab[], int n, int f) {
-        int l = 0, r = n - 1, m = 0;
+        int l = 0, r = n - 1, m;
         while (r - l > 0) {
             m = (l + r) / 2;
             if (tab[m] >= f)
@@ -13,7 +15,10 @@ public class Main {
         return l;
     }
     public static void main(String[] args) {
-        int[] tab = {1, 3, 5, 7, 9, 11, 13, 15};
-        System.out.println(binarySearch(tab, tab.length, 9));
+        int tab1[] = {1, 3, 5, 7, 9, 11, 13, 15};
+        int tab2[] = {3, 9, 11, 4, 1, 20, 15};
+        int tab3[] = {15, 13, 11, 9, 7, 5, 3, 1};
+        System.out.println(binarySearch(tab1, tab1.length, 9));
+        System.out.println(Arrays.toString(Sort.selectionSort(tab2, tab2.length)));
     }
 }
--- src/com/company/Sort.java
+++ src/com/company/Sort.java
@@ -1,4 +1,21 @@
 package com.company;
 
 public class Sort {
+    public static int[] selectionSort(int tab[], int n) {
+        int min, tmp;
+        for (int i = 0; i < n; i++) {
+            min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (tab[j] < tab[min]) {
+                    min = j;
+                }
+            }
+            if (min != i) {
+                tmp = tab[i];
+                tab[i] = tab[min];
+                tab[min] = tmp;
+            }
+        }
+        return tab;
+    }
 }
